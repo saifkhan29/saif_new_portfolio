30d83ebce6c49e0c0ab7d34d4f91bc58e2243fcc
saif khan <saifbusiness29@gmail.com>
2021-11-28 23:46:41 +0530

    my-app

--- src/Pmain.js
+++ src/Pmain.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -8,25 +7,22 @@ import Services from "./components/Services/Services";
 import Youtube from "./components/Youtube/Youtube";
 import Certificate from "./components/Certificates/Certificate";
 import Contact1 from "./components/Contact/Contact1";
-import Pmain from "./Pmain";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <ProjectMain />
-      <Skills_Main />
-      <Services />
-      <Youtube />
-      <Certificate />
-      <Contact1 />
-      
-      
-    </div>
-  );
-}
+import React from 'react'
 
-export default App;
+export default function Pmain() {
+    return (
+        <div>
+            <Navbar/>
+<Home/>
+<About/>
+<ProjectMain/>
+<Skills_Main/>
+<Services/>
+<Youtube/>
+<Certificate/>
+<Contact1/>
+            
+        </div>
+    );
+};
